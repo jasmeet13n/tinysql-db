9da81e60897174d8be7727388067a580f708d0c1
Deep Desai <deepdesai.nitt@gmail.com>
2015-10-28 23:22:53 -0500

    add DatabaseManager files and TinySqlProgram (Main) file

---
+++ TinySQLProgram.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "DatabaseManager.h"
+
+int main() {
+	// Initialize the memory, disk, the database manager and the parser
+  MainMemory mem;
+  Disk disk;
+	DatabaseManager db_manager(&mem, &disk);
+}
