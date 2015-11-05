8e2ec61013c0e195c5664c0584659c9d8946d48d
Jasmeet Singh <jasmeetiiit@gmail.com>
2015-11-04 19:49:40 -0600

    implements insert

--- DatabaseManager.cc
+++ DatabaseManager.cc
@@ -29,13 +29,13 @@ public:
 
   Relation* createTable(ParseTreeNode* root) {
     //Extract Relation Name, Field Names and Field Types
-    string relation_name = Utils::getTableName(root);
+    std::string table_name = Utils::getTableName(root);
     std::vector<std::string> column_names;
     std::vector<enum FIELD_TYPE> data_types;
     Utils::getAttributeTypeList(root, column_names, data_types);
     //Create schema and Create Relation
     Schema schema(column_names, data_types);
-    return schema_manager.createRelation(relation_name,schema);
+    return schema_manager.createRelation(table_name,schema);
   }
 
   bool processCreateTableStatement(ParseTreeNode* root) {
@@ -47,8 +47,48 @@ public:
   }
 
   bool processDropTableStatement(ParseTreeNode* root) {
-    string relation_name = Utils::getTableName(root);
-    return schema_manager.deleteRelation(relation_name);
+    std::string table_name = Utils::getTableName(root);
+    return schema_manager.deleteRelation(table_name);
+  }
+
+  bool processInsertStatement(ParseTreeNode* root) {
+    std::string table_name = Utils::getTableName(root);
+    Relation* r = schema_manager.getRelation(table_name);
+    if (r == nullptr) {
+      return false;
+    }
+
+    // get attribute_list
+    std::vector<std::string> att_list;
+    Utils::getAttributeList(root, att_list);
+
+    // get value_list
+    std::vector<std::string> value_list;
+    Utils::getValueList(root, value_list);
+
+    for (int i = 0; i < att_list.size(); ++i) {
+      std::cout << att_list[i] << " :: " << value_list[i] << std::endl;
+    }
+
+    // get schema from relation
+    Schema s = r->getSchema();
+
+    // get field types from schema
+    for (int i = 0; i < att_list.size(); ++i) {
+      Tuple t = r->createTuple();
+      FIELD_TYPE f = s.getFieldType(att_list[i]);
+      bool result = true;
+      if (f == INT) {
+        int value = stoi(value_list[i], nullptr, 10);
+        result = t.setField(att_list[i], value);
+      } else {
+        result = t.setField(att_list[i], value_list[i]);
+      }
+      if (result == false) {
+        return false;
+      }
+    }
+    return true;
   }
 
   bool processQuery(std::string& query) {
@@ -61,6 +101,8 @@ public:
       return processCreateTableStatement(root);
     } else if (root->type == NODE_TYPE::DROP_TABLE_STATEMENT) {
       return processDropTableStatement(root);
+    } else if (root->type == NODE_TYPE::INSERT_STATEMENT) {
+      return processInsertStatement(root);
     }
     return false;
   }
--- utils.cc
+++ utils.cc
@@ -14,12 +14,35 @@ public:
     std::vector<enum FIELD_TYPE>& data_types) {
     if(node->type == NODE_TYPE::CREATE_TABLE_STATEMENT) {
       getAttributeTypeList(node->children[3], column_names, data_types);
+      return;
     }
     column_names.push_back(node->children[0]->value);
     data_types.push_back(node->children[1]->value == "INT" ? INT : STR20);
     // for CREATE_TABLE_STATEMENT, att_name att_type attribute_type_list
     if(node->children.size() == 3) {
       getAttributeTypeList(node->children[2], column_names, data_types);
+    }
+  }
+
+  static void getAttributeList(ParseTreeNode* node, std::vector<std::string>& att_list) {
+    if (node->type == NODE_TYPE::INSERT_STATEMENT) {
+      getAttributeList(node->children[3], att_list);
+      return;
+    }
+    att_list.push_back(node->children[0]->value);
+    if (node->children.size() == 2) {
+      getAttributeList(node->children[1], att_list);
+    }
+  }
+
+  static void getValueList(ParseTreeNode* node, std::vector<std::string>& value_list) {
+    if (node->type == NODE_TYPE::INSERT_STATEMENT) {
+      getValueList(node->children[4]->children[1], value_list);
+      return;
+    }
+    value_list.push_back(node->children[0]->value);
+    if (node->children.size() == 2) {
+      getValueList(node->children[1], value_list);
     }
   }
 
